56f5c15ae5977c615c96337d67dca97de5d1fb38
yeseong31 <yeseong31@naver.com>
2022-10-19 11:58:39 +0900

    1019 refactor: log-in static 적용 test

--- ch03_flask_bootstrap/login_test.py
+++ ch03_flask_bootstrap/login_test.py
@@ -1,16 +1,16 @@
 from flask import Flask, jsonify, request, render_template
+
 app = Flask(__name__, static_url_path='/static')
 session_count = 0
 
 
 @app.route('/login')
 def login():
-    email_address = request.args.get('email_address')
-    passwd = request.args.get('passwd')
-    print(email_address, passwd)
+    email = request.args.get('email')
+    password = request.args.get('password')
+    print(email, password)
 
-    if email_address == 'dave@gmail.com' and passwd == '111':
-
+    if email == 'dave@gmail.com' and password == '111':
         return_data = {'auth': 'success'}
     else:
         return_data = {'auth': 'failed'}
@@ -19,7 +19,6 @@ def login():
 
 @app.route('/html_test')
 def hello_html():
-    # html file은 templates 폴더에 위치해야 함
     return render_template('login_rawtest.html')
 
 
@@ -39,4 +38,4 @@ def blog_html():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="8080")
+    app.run(host="127.0.0.1", port=5000)
