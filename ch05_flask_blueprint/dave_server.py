6db92be0dcabe57556dea6963baf72bac4c97b6d
yeseong31 <yeseong31@naver.com>
2022-11-07 11:02:45 +0900

    refactor: Blueprint test

--- ch05_flask_blueprint/dave_server.py
+++ ch05_flask_blueprint/dave_server.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 app.register_blueprint(blog_test.blog_ab, url_prefix='/blog')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='8080')
+    app.run(host='0.0.0.0', port=8080)
--- ch05_flask_blueprint/sub_blueprint/blog_test.py
+++ ch05_flask_blueprint/sub_blueprint/blog_test.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 
 blog_ab = Blueprint('blog', __name__)
 
+
 # http://localhost:8080/blog/blog1
 @blog_ab.route('/blog1')
 def blog():
