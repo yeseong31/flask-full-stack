8a95dcb9c368ef24be6111d01e27375e2ccf37f8
yeseong31 <yeseong31@naver.com>
2022-11-14 21:45:42 +0900

    feat: Session 저장 및 삭제 구현
    email 구독을 통해 사용자 Session 생성, 구독 취소를 통해 사용자 Session 삭제

--- flask_ABTest/views/blog_views.py
+++ flask_ABTest/views/blog_views.py
@@ -1,9 +1,18 @@
 from flask import Blueprint, url_for, render_template
+from flask_login import current_user, logout_user
 from werkzeug.utils import redirect
 
 bp = Blueprint('blog', __name__, url_prefix='/')
 
 
-@bp.route('/test')
-def test():
+@bp.route('/')
+def main():
+    if current_user.is_authenticated:
+        return render_template('blog_A.html', email=current_user.email)
     return render_template('blog_A.html')
+
+
+@bp.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('blog.main'))
--- flask_ABTest/views/email_views.py
+++ flask_ABTest/views/email_views.py
@@ -1,5 +1,10 @@
+import datetime
+
 from flask import Blueprint, url_for, render_template, make_response, jsonify, request
+from flask_login import login_user
 from werkzeug.utils import redirect
+
+from flask_ABTest.controls.user_mgmt import User
 
 bp = Blueprint('email', __name__, url_prefix='/email')
 
@@ -8,8 +13,14 @@ bp = Blueprint('email', __name__, url_prefix='/email')
 def subscribe():
     if request.method == 'GET':
         print(f'subscribe: {request.args.get("email")}')
-        return redirect(url_for('blog.test'))
+        return redirect(url_for('blog.main'))
     elif request.method == 'POST':
         # print(f'subscribe: {request.get_json()}')  # content-type이 application/json인 경우
         print(f'subscribe: {request.form.get("email")}')  # content-type이 form 관련 형태인 경우
-        return redirect(url_for('blog.test'))
+
+        user = User.create(request.form.get('email'), 'A')
+
+        # https://docs.python.org/3/library/datetime.html#timedelta-objects
+        login_user(user, remember=True, duration=datetime.timedelta(days=365))  # Remenber Me 활성화
+
+        return redirect(url_for('blog.main'))
