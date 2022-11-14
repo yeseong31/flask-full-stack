c64ba8bba0efa20bcc6fd9dc9f7434bf7bcacd8c
yeseong31 <yeseong31@naver.com>
2022-11-14 21:08:58 +0900

    fix: url 접근 수정
    blog.test로 접근하지 못하는 문제 수정

--- flask_ABTest/views/blog_views.py
+++ flask_ABTest/views/blog_views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, url_for, render_template
 from werkzeug.utils import redirect
 
-bp = Blueprint('main', __name__, url_prefix='/')
+bp = Blueprint('blog', __name__, url_prefix='/')
 
 
 @bp.route('/test')
